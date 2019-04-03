1c60b258e299e4047e6fa46a1e21f77b7e76a018
maitao <814544428@qq.com>
2019-04-03 18:52:35 +0800

    修改启动类

--- springboot-module/src/main/java/com/mt/module/SpringbootModuleApplication.java
+++ springboot-module/src/main/java/com/mt/module/SpringbootModuleApplication.java
@@ -2,7 +2,11 @@ package com.mt.module;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * 
+ * @author maitao
+ *
+ */
 @SpringBootApplication
 public class SpringbootModuleApplication {
 
