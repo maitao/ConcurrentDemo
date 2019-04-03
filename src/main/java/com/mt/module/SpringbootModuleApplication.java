2eabb33fa55f8e8e4c081b7c8819828f0cff7038
maitao <814544428@qq.com>
2019-04-03 18:06:39 +0800

    测试高并发http请求

---
+++ src/main/java/com/mt/module/SpringbootModuleApplication.java
@@ -0,0 +1,13 @@
+package com.mt.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootModuleApplication.class, args);
+	}
+
+}
